2133c0acc178c9b485b1db3e6cd4bd07ae6af273
johnburetdev <johnburet.dev@gmail.com>
2025-04-06 01:06:53 -0400

    Imagenes optimizadas

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 const { src, dest, watch, series } = require("gulp");
+
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
 
 function css(done) {
   src("src/scss/app.scss")
@@ -12,10 +16,23 @@ function css(done) {
   done();
 }
 
+function imagenes() {
+  return src("src/img/**/*")
+    .pipe(imagemin({ optimizationLevel: 3 }))
+    .pipe(dest("build/img"));
+}
+
+function versionWebp() {
+  return src("src/img/**/*.{jpg, png}").pipe(webp()).pipe(dest("build/img"));
+}
+
 function dev() {
   watch("src/scss/**/*.scss", css);
+  watch("src/img/**/*", imagenes);
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css, dev);
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.default = series(imagenes, versionWebp, css, dev);
