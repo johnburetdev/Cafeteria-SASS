8c3b4430fc0443e2725096103523b310a42b72ff
johnburetdev <johnburet.dev@gmail.com>
2025-04-08 23:53:01 -0400

    Mixins + snippets de media queries creados

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,19 @@ const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 
-const imagemin = require("gulp-imagemin");
+/*const imagemin = require("gulp-imagemin");*/
 const webp = require("gulp-webp");
 
-function css(done) {
-  src("src/scss/app.scss")
+function css() {
+  return src("src/scss/app.scss")
     .pipe(sass())
     .pipe(postcss([autoprefixer()]))
     .pipe(dest("build/css"));
-
-  done();
 }
 
-function imagenes() {
-  return src("src/img/**/*")
-    .pipe(imagemin({ optimizationLevel: 3 }))
-    .pipe(dest("build/img"));
-}
+// function imagenes() {
+//  return src("src/img/**/*").pipe(imagemin()).pipe(dest("build/img"));
+//}
 
 function versionWebp() {
   return src("src/img/**/*.{jpg, png}").pipe(webp()).pipe(dest("build/img"));
@@ -28,11 +24,11 @@ function versionWebp() {
 
 function dev() {
   watch("src/scss/**/*.scss", css);
-  watch("src/img/**/*", imagenes);
+  watch("src/img/**/*", versionWebp);
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.imagenes = imagenes;
+//exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.default = series(imagenes, versionWebp, css, dev);
+exports.default = series(versionWebp, css, dev);
